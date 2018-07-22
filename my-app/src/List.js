2b9b8e469b39a7e0da678fa1d104dc373d3e21ca
Tamara Kovalchuk <kovalchuktamara94@gmail.com>
2018-07-22 22:20:41 +0300

    Add edit-inline
    fix list js and css

--- my-app/src/List.js
+++ my-app/src/List.js
@@ -1,7 +1,22 @@
 import React, { Component } from 'react';
 import Edit from './Edit';
+import { RIEInput } from 'riek'
+
 
 class List extends Component {
+
+  constructor(props) {
+    super(props);
+    this.getDataChangedHandler = this.getDataChangedHandler.bind(this);
+  }
+
+  getDataChangedHandler(student) {
+    return (data) => {
+      console.log(data);
+      student.nameStudent = data.inputValue;
+      this.props.editStudentCallback(student);
+    }
+  }
 
   render() {
       return (
@@ -9,7 +24,13 @@ class List extends Component {
                   <h1>Lv-330.Node.js</h1>
                   <ol>
                     {this.props.todos.map(stud =>(<li key={stud.id}>
-                      {stud.nameStudent}
+
+                      <RIEInput
+                        value={stud.nameStudent}
+                        propName='inputValue'
+                        change={this.getDataChangedHandler(stud)}
+                        classEditing = "classEditing"
+                      />
                     <div className="button">
                     <i className="material-icons">edit</i>
                     <Edit editStudent={stud}
@@ -33,7 +54,7 @@ class List extends Component {
         return () => {
           this.props.deleteStudentCallback(item);
         };
-  }
+      }
 }
 export default List;
 
