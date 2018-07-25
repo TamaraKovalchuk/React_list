489a983c88190fe9b9b9dd49e4b16139976910af
Tamara Kovalchuk <kovalchuktamara94@gmail.com>
2018-07-25 19:48:41 +0300

    add

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,48 +1,103 @@
 import React, { Component } from "react";
+import axios from "axios"
 import List from "./List";
 import Add from "./Add";
 import "./css/App.css";
 
 class App extends Component {
+
+    axiosInst = axios.create({
+      baseURL: 'http://localhost:4000',
+      timeout: 5000
+    });
+
   state = {
     inputValue: "initial input",
     todos: [
-      { id: 1, nameStudent: "Tamara Kovalchuk" },
-      { id: 2, nameStudent: "Anna Dron" },
-      { id: 3, nameStudent: "Maksym Javir" },
-      { id: 4, nameStudent: "Andrii Korin" },
-      { id: 5, nameStudent: "Iurii Besidka" },
-      { id: 6, nameStudent: "Vitalii Dvorian" },
-      { id: 7, nameStudent: "Volodja Pitchuk" },
-      { id: 8, nameStudent: "Artur Khomenko" }
     ]
   };
 
+  componentDidMount() {
+    this.getTodos();
+  }
+
   addNewStudent = studentName => {
-    this.state.todos.push({
-      id: this.state.todos.length + 1,
+    let _this = this;
+    this.axiosInst.post('/', {
       nameStudent: studentName
-    });
-    this.setState({
-      todos: this.state.todos
+    })
+    .then(function(response) {
+      _this.state.todos.push(response.data);
+      _this.setState({
+        todos: _this.state.todos
+      });
+    })
+    .catch(function (error) {
+        _this.handleError(error);
     });
   };
 
   deleteStudent = student => {
+    let _this = this;
     var delStudent = this.state.todos.indexOf(student);
-    this.state.todos.splice(delStudent, 1);
-    this.setState({
-      todos: this.state.todos
+    this.axiosInst.delete('/delete/' + student.id)
+    .then(function(response) {
+      _this.state.todos.splice(delStudent, 1);
+      _this.setState({
+        todos: _this.state.todos
+      });
+    })
+    .catch(function (error) {
+        _this.handleError(error);
     });
   };
 
   editStudent = student => {
+    let _this = this;
     var editStudent = this.state.todos.indexOf(student);
-    this.state.todos[editStudent].nameStudent = student.nameStudent;
-    this.setState({
-      todos: this.state.todos
+
+    this.axiosInst.post('/edit/' + student.id, {
+      nameStudent: student.nameStudent
+    })
+    .then(function(response) {
+      _this.state.todos[editStudent].nameStudent = student.nameStudent;
+      _this.setState({
+        todos: _this.state.todos
+      });
+    })
+    .catch(function (error) {
+        _this.handleError(error);
     });
   };
+
+  getTodos = () => {
+    let _this = this;
+    // GET request for remote image
+    this.axiosInst.get('/users')
+    .then(function(response) {
+      // response.data.pipe(fs.createWriteStream('ada_lovelace.jpg'))
+      _this.setState({
+        todos: response.data.teammates
+      });
+    })
+    .catch(function (error) {
+        _this.handleError(error);
+      });
+    return [];
+  }
+
+  handleError = (error) => {
+    if (error.response) {
+        console.log(error.response.data);
+        console.log(error.response.status);
+        console.log(error.response.headers);
+      } else if (error.request) {
+        console.log(error.request);
+      } else {
+        console.log('Error', error.message);
+      }
+      console.log(error);
+  }
 
   render() {
     return (
--- my-app/src/List.js
+++ my-app/src/List.js
@@ -1,6 +1,11 @@
 import React, { Component } from 'react';
 import Edit from './Edit';
 import { RIEInput } from 'riek'
+import Pagination from "react-js-pagination";
+import { Router, Route, Switch } from 'react-router';
+var Router = require('react-router').Router
+var Route = require('react-router').Route
+var Switch = require('react-router').Switch
 
 
 class List extends Component {
@@ -8,6 +13,20 @@ class List extends Component {
   constructor(props) {
     super(props);
     this.getDataChangedHandler = this.getDataChangedHandler.bind(this);
+    this.state = {
+      itemsPerPage: 3,
+      activePage: 1,
+      renderedUsers: this.props.todos,
+      totalItemsCount: 0
+    };
+  }
+
+  handlePageChange = (pageNumber) => {
+    const renderedUsers = this.props.todos
+      .slice((pageNumber - 1) * this.state.itemsPerPage,
+        pageNumber * this.state.itemsPerPage);
+    console.log(`active page is ${pageNumber}`);
+    this.setState({activePage: pageNumber, renderedUsers: renderedUsers});
   }
 
   getDataChangedHandler(student) {
@@ -18,12 +37,19 @@ class List extends Component {
     }
   }
 
+  static getDerivedStateFromProps(props, state) {
+    state.renderedUsers = props.todos
+      .slice((state.activePage - 1) * state.itemsPerPage,
+        state.activePage * state.itemsPerPage);
+    state.totalItemsCount = props.todos.length;
+  }
+
   render() {
       return (
                 <div>
                   <h1>Lv-330.Node.js</h1>
                   <ol>
-                    {this.props.todos.map(stud =>(<li key={stud.id}>
+                    {this.state.renderedUsers.map(stud =>(<li key={stud.id}>
 
                       <RIEInput
                         value={stud.nameStudent}
@@ -45,6 +71,14 @@ class List extends Component {
                     </div>
                       </li>))}
                   </ol>
+                  <Pagination
+                      activePage={this.state.activePage}
+                      itemsCountPerPage={this.state.itemsPerPage}
+                      totalItemsCount={this.state.totalItemsCount}
+                      pageRangeDisplayed={5}
+                      onChange={this.handlePageChange}
+                      style = "display: flex"
+                    />
                 </div>
 
               );
